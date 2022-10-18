855aa40eda6d409a836d732ccb0bf5710ca68635
Mu-jun <suo24691@gmail.com>
2022-10-18 17:35:51 +0900

    i18n test 완료

---
+++ i18n.js
@@ -0,0 +1,23 @@
+//모듈 불러오기
+const i18n = require('i18n');
+
+//옵션 설정
+i18n.configure({
+    //사용언어 설정 
+    locales: ['ko', 'en'],
+
+    //언어를 설정한 json 파일 생성위치 - 기본은 ./locales
+    directory: __dirname + '/locales',
+
+    //기본 사용언어 설정
+    defaultLocale: 'ko',
+
+    //사용언어를 저장할 cookie 이름
+    cookie: 'lang'
+});
+
+module.exports = (req, res, next) => {
+    i18n.init(req, res);
+    res.locals.__ = res.__;  // ejs에서 사용시 추가
+    return next();
+};
