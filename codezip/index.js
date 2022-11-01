89882b1f55581fad597c0e8fc3e8aaa11b94388c
Mu-jun <suo24691@gmail.com>
2022-11-01 18:14:18 +0900

    샘플의뢰서 기본 UI 구현

--- codezip/index.js
+++ codezip/index.js
@@ -1,8 +1,10 @@
 const country = require('./country');
 const url = require('./url.json')
+const numberOfProduct = require('./numberOfProduct.json')
 const codezip = {};
 
 codezip.country = country;
 codezip.url = url;
+codezip.numberOfProduct = numberOfProduct;
 
 module.exports = codezip;
